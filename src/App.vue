23bd0e8d95a2630e813eaabf11dd9acb1837c97c
Alex88Larionova <sasha.larionova.92@mail.ru>
2022-09-27 11:05:35 +0300

    Правки по стилям

--- src/App.vue
+++ src/App.vue
@@ -7,6 +7,7 @@ import AppNoteEditor from "./components/AppNoteEditor/AppNoteEditor.vue";
 const showNoteEditor = ref(false);
 const search = ref("");
 const notes = ref([]);
+/* const notes = ref(JSON.parse(localStorage.getItem("notes"))) || ref([]); */
 const filteredNoted = computed(() =>
   notes.value.filter((note) => noteFilter(note))
 );
@@ -23,12 +24,25 @@ function noteFilter(note) {
 <template>
   <div class="app__title">Notes</div>
   <AppSearch :test="search" />
-  <AppNotes :filteredNoted="filteredNoted" :notes="notes" />
-  <AppNoteEditor v-if="showNoteEditor" :notes="notes" :showNoteEditor="showNoteEditor" @closeNote="showNoteEditor = false"/>
-  <button class="app__note-editor-button" @click="showNoteEditor = true">+</button>
+  <AppNotes
+    :filteredNoted="filteredNoted"
+    :notes="notes"
+    :showNoteEditor="showNoteEditor"
+  />
+  <AppNoteEditor
+    v-if="showNoteEditor"
+    :notes="notes"
+    :showNoteEditor="showNoteEditor"
+    @closeNote="showNoteEditor = false"
+  />
+  <button class="app__note-editor-button" @click="showNoteEditor = true">
+    +
+  </button>
 </template>
 
 <style>
+@import url("https://fonts.googleapis.com/css2?family=Roboto:wght@300&display=swap");
+
 :root {
   --app-background-color: rgb(14, 18, 27);
   --app-buttons-main-color: rgb(51, 105, 255);
@@ -36,6 +50,10 @@ function noteFilter(note) {
   --app-notes-shadow-color: rgb(37, 40, 78);
   --app-input-background-color: rgb(23, 28, 38);
   --app-text-color: rgb(228, 227, 227);
+}
+
+* {
+  font-family: "Roboto", sans-serif;
 }
 
 body {
@@ -57,12 +75,12 @@ body {
 }
 
 .app__note-editor-button {
+  text-align: center;
   position: fixed;
   right: 20px;
   bottom: 20px;
   cursor: pointer;
-  margin-right: auto;
-  margin-right: 20px;
+  margin-right: 0;
   background-color: var(--app-buttons-main-color);
   color: var(--app-text-color);
   font-size: 24px;
@@ -71,19 +89,6 @@ body {
   height: 50px;
   border-radius: 50%;
   box-shadow: 5px 5px 15px var(--app-notes-shadow-color);
-}
-
-.app__note-delete-button {
-  cursor: pointer;
-  z-index: 1;
-  font-size: 14px;
-  margin: 0;
-  padding: 0px 5px;
-  background-color: var(--app-buttons-main-color);
-  border: 3px solid var(--app-buttons-main-color);
-  border-radius: 50%;
-  color: rgb(228, 227, 227);
-  box-shadow: 2px 2px 5px var(--app-buttons-shadow-color);
 }
 
 .app__note-delete-button:hover {
